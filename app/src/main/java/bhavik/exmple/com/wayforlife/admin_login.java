708b63d4d5c51713484834d89454b302a53bafd4
Bhavik Vashi <bhavikvashi1804@gmail.com>
2019-01-20 15:58:09 +0530

    Event Activity

--- app/src/main/java/bhavik/exmple/com/wayforlife/admin_login.java
+++ app/src/main/java/bhavik/exmple/com/wayforlife/admin_login.java
@@ -1,13 +1,30 @@
 package bhavik.exmple.com.wayforlife;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class admin_login extends AppCompatActivity {
+    FirebaseDatabase database;
+    ProgressDialog pd;
+    Boolean check=false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,17 +32,44 @@ public class admin_login extends AppCompatActivity {
         setContentView(R.layout.activity_admin_login);
 
 
-        EditText uname= (EditText)findViewById(R.id.admin_loging_uname);
-        EditText upassword=(EditText)findViewById(R.id.admin_loging_upw);
+        final EditText uname= (EditText)findViewById(R.id.admin_loging_uname);
+        final EditText upassword=(EditText)findViewById(R.id.admin_loging_upw);
 
         Button loging=(Button)findViewById(R.id.adminloging_loging);
         loging.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //loging logic
+                pd=new ProgressDialog(admin_login.this);
+                pd.show();
+                pd.setMessage("Authenticating...");
 
-                Intent i=new Intent(getApplicationContext(),uploadEvent.class);
-                startActivity(i);
+                final String cUserName=uname.getText().toString();
+                final String cPassWord=upassword.getText().toString();
+
+
+                fetch(cUserName,cPassWord);
+
+
+
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                if(check)
+                {
+                    Intent intent=new Intent(getApplicationContext(),uploadEvent.class);
+                    startActivity(intent);
+                    pd.dismiss();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Username/ password doesnot match",Toast.LENGTH_SHORT).show();
+                }
+
+
+
+
             }
         });
 
@@ -33,9 +77,80 @@ public class admin_login extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String username=uname.getText().toString();
+                String password=upassword.getText().toString();
+
+                if(username.isEmpty() && password.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(),"Please enter the username & password",Toast.LENGTH_SHORT).show();
+
+                }
+                else
+                {
+
+                    pd=new ProgressDialog(admin_login.this);
+                    pd.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                    pd.setTitle("Updating Database");
+                    pd.setProgress(0);
+                    pd.show();
+
+                    DatabaseReference r = database.getInstance().getReference();
+
+                    r.child("Admins").child(username).setValue(password).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if (task.isSuccessful()) {
+                                pd.dismiss();
+                                Toast.makeText(getApplicationContext(), "Database Updated", Toast.LENGTH_SHORT).show();
+
+                            }else
+                                Toast.makeText(getApplicationContext(), "Database entry is not created", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                }
+
                 //send email
             }
         });
     }
+
+    private void fetch(String cUserName,String cPassWord)
+    {
+        final String name=cUserName,pw=cPassWord;
+
+        DatabaseReference r=FirebaseDatabase.getInstance().getReference();
+
+
+        r.child("Admins").addChildEventListener(new ChildEventListener() {
+
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+
+                if(name.equals(dataSnapshot.getKey()) && pw.equals(dataSnapshot.getValue(String.class)))
+                {
+                    check=true;
+                }
+
+            }
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            }
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+            }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+    }
+
 
 }
--- app/src/main/java/bhavik/exmple/com/wayforlife/uploadEvent.java
+++ app/src/main/java/bhavik/exmple/com/wayforlife/uploadEvent.java
@@ -1,29 +1,105 @@
 package bhavik.exmple.com.wayforlife;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class uploadEvent extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_event);
+        pd=new ProgressDialog(getApplicationContext());
 
-        EditText ename=(EditText)findViewById(R.id.upload_event_ename);
-        EditText eplace=(EditText)findViewById(R.id.upload_event_eplace);
-        EditText eDesc=(EditText)findViewById(R.id.upload_event_eDesc);
-        EditText eDate=(EditText)findViewById(R.id.upload_event_eDate);
-        EditText eDur=(EditText)findViewById(R.id.upload_event_eDur);
+        final EditText ename=(EditText)findViewById(R.id.upload_event_ename);
+        final EditText eplace=(EditText)findViewById(R.id.upload_event_eplace);
+        final EditText eDesc=(EditText)findViewById(R.id.upload_event_eDesc);
+        final EditText eDate=(EditText)findViewById(R.id.upload_event_eDate);
+        final EditText eDur=(EditText)findViewById(R.id.upload_event_eDur);
+
+
+
 
         Button createEvent=(Button)findViewById(R.id.upload_event_create);
+
         createEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
+
+                String ename1=ename.getText().toString();
+                String eplace1=eplace.getText().toString();
+                String edesc1=eDesc.getText().toString();
+                String edate=eDate.getText().toString();
+                String edur=eDur.getText().toString();
+
+                // Write a message to the database
+
+
+                Date currentTime = Calendar.getInstance().getTime();
+                String fDate = new SimpleDateFormat("yyyy-MM-dd").format(currentTime);
+                String cTime=new SimpleDateFormat("h:mm a").format(currentTime);
+
+
+
+                String EventID=fDate+cTime;
+
+
+                if(ename1.isEmpty() && edesc1.isEmpty() && eplace1.isEmpty()  && edate.isEmpty() &&  edur.isEmpty()) {
+
+                    Toast.makeText(getApplicationContext(),"Please fill all the information",Toast.LENGTH_SHORT).show();
+
+
+                }
+
+                else {
+
+                    pd=new ProgressDialog(uploadEvent.this);
+                    pd.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                    pd.setTitle("Updating Database");
+                    pd.setProgress(0);
+                    pd.show();
+
+
+                    DatabaseReference r = database.getInstance().getReference();
+                    r.child("EventInfo").child(EventID).child("Event Name").setValue(ename1);
+                    r.child("EventInfo").child(EventID).child("Event Place").setValue(eplace1);
+                    r.child("EventInfo").child(EventID).child("Event Desc").setValue(edesc1);
+                    r.child("EventInfo").child(EventID).child("Event Date").setValue(edate);
+                    r.child("EventInfo").child(EventID).child("Event Dur").setValue(edur).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if (task.isSuccessful()) {
+                                pd.dismiss();
+                                 Toast.makeText(getApplicationContext(), "Database Updated", Toast.LENGTH_SHORT).show();
+
+                            }else
+                                Toast.makeText(getApplicationContext(), "Database entry is not created", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
             }
         });
 
