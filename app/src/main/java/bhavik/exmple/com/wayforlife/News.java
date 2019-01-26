cbbbfe41536da3550d533777b3ac867e2079a230
Srinivas Gunti <srinivasgunti987@gmail.com>
2019-01-26 15:50:03 +0530

    Update News.java

--- app/src/main/java/bhavik/exmple/com/wayforlife/News.java
+++ app/src/main/java/bhavik/exmple/com/wayforlife/News.java
@@ -1,13 +1,39 @@
 package bhavik.exmple.com.wayforlife;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class News extends AppCompatActivity {
+    Button discussions, polls, complaints;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        discussions =findViewById(R.id.discussions);
+        polls =findViewById(R.id.polls);
+        complaints =findViewById(R.id.complaints);
+
+        discussions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(getApplicationContext(),discussions.class);
+                startActivity(i);
+            }
+        });
+
+
+        complaints.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent k=new Intent(getApplicationContext(),complaints.class);
+                startActivity(k);
+            }
+        });
+
     }
 }
